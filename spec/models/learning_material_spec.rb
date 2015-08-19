2398c93e6880ca41957a63b09435c8c54603408e
F3PiX <F3PiXnl@gmail.com>
2015-08-19 11:39:20 +0200

    Adding model specs

--- spec/models/learning_material_spec.rb
+++ spec/models/learning_material_spec.rb
@@ -1,5 +1,37 @@
 require 'rails_helper'
 
-RSpec.describe LearningMaterial, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe LearningMaterial, type: :model do
+
+  context 'with valid data' do
+
+    it 'has a valid factory' do
+      expect(build(:learning_material)).to be_valid
+    end
+
+    it 'is valid with validated fields filled in' do
+      learning_material = build(:learning_material)
+      expect(learning_material).to be_valid
+    end
+
+    it 'is valid with blank non-validated fields' do
+      learning_material = build(:learning_material, level: nil, description: nil)
+      expect(learning_material).to be_valid
+    end
+  end
+
+
+  context 'with invalid data' do
+
+    it 'is invalid without a topic' do
+      learning_material = build(:learning_material, topic: nil)
+      expect(learning_material).not_to be_valid
+      expect(learning_material.errors[:topic]).to include("can't be blank")
+    end
+
+    it 'is invalid without a source' do
+      learning_material = build(:learning_material, source: nil)
+      expect(learning_material).not_to be_valid
+      expect(learning_material.errors[:source]).to include("can't be blank")
+    end
+  end
 end
